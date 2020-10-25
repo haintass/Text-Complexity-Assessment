bb7298f0b7690af1e263ec27c83850b407c9d19f
evgeny-kulakov <haintass@gmail.com>
2020-10-25 21:33:32 +0300

    added cors policy settings, updated proxy configuration

--- server/main.py
+++ server/main.py
@@ -1,1 +1,20 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+# settings cors policy
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', 'http://localhost:4200')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  response.headers.add('Access-Control-Allow-Credentials', 'true')
+  return response
+
+if __name__ == "__main__":
+    app.run(port=3000)
+
+
+@app.route('/api/test')
+def test():
+    return 'test a request'
