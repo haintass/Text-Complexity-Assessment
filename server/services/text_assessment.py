763c381ae3742cd265a0f1e456595271b522922b
evgeny-kulakov <haintass@gmail.com>
2021-01-06 23:12:50 +0300

    updated the Flesh reading ease scale, added additional comments

--- server/services/text_assessment.py
+++ server/services/text_assessment.py
@@ -6,33 +6,43 @@ from enums.regex_patterns import PerfectContinuousTensePatterns, PerfectTensePat
 
 def calculate_text_complexity(text):
     text_without_non_alphabet_chars = _remove_non_alphabet_chars(text)
-    sentences = _get_sentences(text)
+    sentences = _split_text_into_sentences(text)
     sentences_count = len(sentences)
     total_word_count = _count_all_words(sentences)
 
-    average_sentence_length, average_word_length = _calc_average_length_of_words_and_sentences(
-        text_without_non_alphabet_chars, sentences_count, total_word_count
-    )
+    average_sentence_length = _calc_average_sentence_length(total_word_count, sentences_count)
+    average_word_length = _calc_average_word_length(total_word_count, text_without_non_alphabet_chars)
 
     flesch_reading_ease_scale = _get_flesch_reading_ease_scale(average_sentence_length, average_word_length, sentences_count)
-    common_word_rating = _calc_rate_of_common_words_in_sentences(text_without_non_alphabet_chars, total_word_count)
-    text_uniqueness_rating = _get_text_uniqueness_rating(sentences, sentences_count)
+    common_words_scale = _get_occurrence_of_common_words_scale(text_without_non_alphabet_chars, total_word_count)
+    text_repeatability_scale = _get_text_repeatability_scale(sentences, sentences_count)
 
     perfect_continuous_sentences_rating = _calc_occurrence_of_perfect_continuous_sentences(text, sentences_count)
     perfect_sentences_rating = _calc_occurrence_of_perfect_sentences(text, sentences_count)
 
-    result = (flesch_reading_ease_scale + common_word_rating + text_uniqueness_rating + perfect_continuous_sentences_rating + perfect_sentences_rating) / 5
+    result = _calc_text_complexity(flesch_reading_ease_scale, common_words_scale, text_repeatability_scale,
+                                   perfect_continuous_sentences_rating, perfect_sentences_rating)
 
-    return round(result) if result <= 100 else 100
+    return result
+
+
+def _calc_text_complexity(flesch_reading_ease_scale, common_words_scale, text_repeatability_scale,
+                          perfect_continuous_sentences_rating, perfect_sentences_rating):
+    result = (flesch_reading_ease_scale + common_words_scale + text_repeatability_scale + (
+                perfect_continuous_sentences_rating + perfect_sentences_rating) * 0.5) / 4
+
+    # the result should be between 0 and 100
+    return round(result) if 0 < result < 100 else 0 if result < 0 else 100
 
 
 def _calc_occurrence_of_perfect_continuous_sentences(text, sentences_count):
-    # the number of all interrogative sentences of present / past / future perfect continuous tense
+    # all interrogative sentences of the present, past and future perfect continuous tenses
     all_interrogative_sentences = re.findall(
         pattern=PerfectContinuousTensePatterns.all_interrogative_sentences_pattern,
         string=text,
         flags=re.I | re.M)
-    # the number of all affirmative and negative sentences of present perfect continuous tense
+
+    # all affirmative and negative sentences of present perfect continuous tense
     all_affirmative_and_negative_sentences = re.findall(
         pattern=PerfectContinuousTensePatterns.all_affirmative_and_negative_sentences_pattern,
         string=text,
@@ -46,12 +56,14 @@ def _calc_occurrence_of_perfect_continuous_sentences(text, sentences_count):
 
 
 def _calc_occurrence_of_perfect_sentences(text, sentences_count):
+    # all interrogative sentences of the present, past and future perfect tenses
     all_interrogative_sentences = re.findall(
         pattern=PerfectTensePatterns.all_interrogative_sentences_pattern,
         string=text,
         flags=re.I | re.M
     )
 
+    # all affirmative and negative sentences of present perfect tense
     all_affirmative_and_negative_sentences = re.findall(
         pattern=PerfectTensePatterns.all_affirmative_and_negative_sentences_pattern,
         string=text,
@@ -65,25 +77,33 @@ def _calc_occurrence_of_perfect_sentences(text, sentences_count):
 
 
 def _get_flesch_reading_ease_scale(average_sentence_length, average_word_length, sentences_count):
-    sentence_length_scale = 112 + (50 - average_sentence_length) - (math.log(math.pow(average_sentence_length, 3)) * 5)
+    """
+    calculates the Flesch reading ease scale to indicate how difficult a text in English is to understand
+    :param average_sentence_length:
+    :param average_word_length:
+    :param sentences_count:
+    :return: returns a number indicating the difficulty of reading English text.
+     The larger the number, the easier it is to read the text.
+    """
+    sentence_length_scale = 120 - (math.log(math.pow(average_sentence_length, 5)) * 7)
     word_length_scale = 105 - math.pow(average_word_length, 2)
 
-    return (sentence_length_scale + word_length_scale - (0 if sentences_count < 45 else math.log2(sentences_count) * 10)) / 3
+    return (sentence_length_scale + word_length_scale - (0 if sentences_count < 45 else math.log2(sentences_count) * 10)) / math.log2(average_word_length)
 
 
-def _get_text_uniqueness_rating(sentences, sentences_count):
-    frequency_of_repeated_sentences = _calc_frequency_of_repeated_sentences(sentences, sentences_count)
-
-    return frequency_of_repeated_sentences + 30
-
-
-def _calc_frequency_of_repeated_sentences(sentences, sentences_count):
+def _get_text_repeatability_scale(sentences, sentences_count):
+    # calculates the percentage of duplicate sentences
     number_of_unique_sentences = len(set(sentences))
-
     return 100 - (number_of_unique_sentences / sentences_count * 100)
 
 
-def _calc_rate_of_common_words_in_sentences(text, total_word_count):
+def _get_occurrence_of_common_words_scale(text, total_word_count):
+    """
+    calculates the percentage of the most common English words in the text
+    :param text:
+    :param total_word_count:
+    :return: a number between 0 and 100
+    """
     common_word_count = 0
     all_text_words = re.split(' ', text)
     for word in all_text_words:
@@ -93,22 +113,15 @@ def _calc_rate_of_common_words_in_sentences(text, total_word_count):
     return common_word_count / total_word_count * 100
 
 
-def _calc_average_length_of_words_and_sentences(text_without_non_alphabet_chars, sentences_count, total_word_count):
-    average_sentence_length = _calc_average_sentence_length(total_word_count, sentences_count)
-    average_word_length = _calc_average_word_length(total_word_count, text_without_non_alphabet_chars)
-
-    return average_sentence_length, average_word_length
-
-
-def _get_sentences(text):
+def _split_text_into_sentences(text):
     # delete extra whitespaces
     text = re.sub(' +', ' ', text).strip()
 
     # split text into sentences
-    sentences = re.split('\n|\\.|\\.|\\!|\\?|\\;', text)
+    sentences = re.split('\n|\\.|\\!|\\?|\\;', text)
 
-    # delete empty sentences
-    sentences = [s for s in sentences if len(s) > 0 and not s.isspace()]
+    # remove empty sentences and sentences containing only numbers
+    sentences = [s for s in sentences if len(s) > 0 and not s.isspace() and not re.match('^[\d.]+$', s)]
 
     return sentences
 
@@ -127,4 +140,5 @@ def _calc_average_word_length(total_word_count: int, text_without_non_alphabet_c
 
 
 def _remove_non_alphabet_chars(text):
+    # replaces all non-numeric and non-alphabetic characters with spaces
     return re.sub('[^0-9a-zA-Z]+', ' ', text)
